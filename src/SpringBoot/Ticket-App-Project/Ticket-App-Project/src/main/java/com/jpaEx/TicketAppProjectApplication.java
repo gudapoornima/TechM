1143fafd8f0e4049d9ce18cf2dbde375a65f66d8
gudapoornima <gudapoornima1934@gmail.com>
2025-03-18 19:57:24 +0530

    Add files via upload

---
+++ src/SpringBoot/Ticket-App-Project/Ticket-App-Project/src/main/java/com/jpaEx/TicketAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jpaEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAppProjectApplication.class, args);
+	}
+
+}
