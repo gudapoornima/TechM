d260f0564e232bc86ecb38760f1e0894788b9a9b
gudapoornima <gudapoornima1934@gmail.com>
2025-04-30 19:01:18 +0530

    Delete TestProject_2 directory

--- TestProject_2/src/main/java/com/test/repository/CartRepo.java
+++
@@ -1,7 +0,0 @@
-package com.test.repository;
-
-import com.test.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepo extends JpaRepository<Cart, Long> {}
-
--- TestProject_2/src/main/java/com/test/repository/OrderRepo.java
+++
@@ -1,8 +0,0 @@
-package com.test.repository;
-
-import com.test.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order, Long> {}
-
-
